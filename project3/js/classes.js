d6bf049135d6c9930e62bfb07d995ee77da171fc
Emmett1203 <eem1203@g.rit.edu>
2024-05-04 23:22:50 -0400

    final work done

--- project3/js/classes.js
+++ project3/js/classes.js
@@ -10,7 +10,7 @@ class Dot extends PIXI.Sprite {
 
  class Wall extends PIXI.Sprite {
     constructor (x = 0, y = 0){
-        super(app.loader.resources["images/dot.png"].texture);
+        super(app.loader.resources["images/wall.png"].texture);
         this.anchor.set(0.5, 0.5);
         this.scale.set(1);
         this.x = x;
--- project3/js/main.js
+++ project3/js/main.js
@@ -12,6 +12,7 @@ const sceneHeight = app.view.height;
 app.loader.
     add([
         "images/dot.png",
+        "images/wall.png",
     ]);
 app.loader.onProgress.add(e => { console.log(`progress=${e.progress}`) });
 app.loader.onComplete.add(setup);
@@ -20,9 +21,17 @@ app.loader.load();
 let stage;
 
 let startScene;
-let gameScene,dot,wall;
+let gameScene,dot,scoreLabel,wall;
 let gameOverScene;
 
+let speed,score,highscore;
+
+let paused = true;
+
+const prefix = "eem1203-"; // change 'abc1234' to your banjo id
+const highKey = prefix + "high";
+const storedHigh = localStorage.getItem(highKey);
+renderer = PIXI.autoDetectRenderer(width, height);renderer.view.style.position = 'absolute';renderer.view.style.left = Math.floor((baseWidth-width)/2)+'px';renderer.view.style.top = '0px';document.appendChild(renderer.view);
 //only body.appendChild when the page is loaded
 function setup() {
 	stage = app.stage;
@@ -51,21 +60,31 @@ function setup() {
 
     // #8 - Start update loop
 	app.ticker.add(gameLoop);
+
+    speed = 1;
+    score = 0;
+    if (storedHigh){
+        highscore = storedHigh;
+    }else{
+        highscore = 0; // a default value if `nameField` is not found
+    }
+    //Code from https://stackoverflow.com/questions/52097840/how-to-overwrite-text-in-p-tag-using-javascript
+    document.getElementById('replace-me').innerText = "High Score: " + highscore;
 }
 
 function createLabelsAndButtons(){
     let buttonStyle = new PIXI.TextStyle({
-        fill: 0xFF0000,
+        fill: 0xFFFFFF,
         fontSize: 43,
-        fontFamily: "Futura"
+        fontFamily: 'Lineal',
     });
 
     let startLabel1 = new PIXI.Text("Opening in the wall");
     startLabel1.style = new PIXI.TextStyle({
-        fill: 0xFFFFFF,
+        fill: 0x000000,
         fontSize: 59,
-        fontFamily: "Futura",
-        stroke: 0xFF0000,
+        fontFamily: "Gill Sans",
+        stroke: 0xFFFFFF,
         strokeThickness: 6
     });
     startLabel1.x = 50;
@@ -82,6 +101,47 @@ function createLabelsAndButtons(){
     startButton.on('pointerover', e => e.target.alpha = 0.7);
     startButton.on('pointerout', e => e.currentTarget.alpha = 1.0);
     startScene.addChild(startButton);
+
+    let textStyle = new PIXI.TextStyle({
+        fill: 0x000000,
+        fontSize: 18, 
+        fontFamily: "Gill Sans",
+        stroke: 0xFFFFFF,
+        strokeThickness: 4
+    });
+
+    scoreLabel = new PIXI.Text();
+    scoreLabel.style = textStyle;
+    scoreLabel.x = 5;
+    scoreLabel.y = 5;
+    gameScene.addChild(scoreLabel);
+    increaseScoreBy(0);
+
+
+    let gameOverText = new PIXI.Text(" Game Over!");
+    textStyle = new PIXI.TextStyle({
+        fill: 0x000000,
+    	fontSize: 64,
+	    fontFamily: "Gill Sans",
+        stroke: 0xFFFFFF,
+    	strokeThickness: 6
+    });
+    gameOverText.style = textStyle;
+    gameOverText.x = 110;
+    gameOverText.y = sceneHeight/2 - 160;
+    gameOverScene.addChild(gameOverText);
+
+    // 3B - make "play again?" button
+    let playAgainButton = new PIXI.Text("Play Again?");
+    playAgainButton.style = buttonStyle;
+    playAgainButton.x = 190;
+    playAgainButton.y = sceneHeight - 100;
+    playAgainButton.interactive = true;
+    playAgainButton.buttonMode = true;
+    playAgainButton.on("pointerup", reloadPage); // startGame is a function reference
+    playAgainButton.on('pointerover',e=>e.target.alpha = 0.7); // concise arrow function with no brackets
+    playAgainButton.on('pointerout',e=>e.currentTarget.alpha = 1.0); // ditto
+    gameOverScene.addChild(playAgainButton);
 }
 
 function startGame(){
@@ -90,12 +150,15 @@ function startGame(){
     gameScene.visible = true;
     dot.x = 20;
     dot.y = 300;
+    speed = 1;
+    score = 0;
     wall.x = 600;
     wall.y = 300;
+    paused = false;
 }
 
 function gameLoop(){
-	//if (paused) return; // keep this commented out for now
+	if (paused) return; // keep this commented out for now
 
 	// #1 - Calculate "delta time"
 	let dt = 1/app.ticker.FPS;
@@ -117,5 +180,46 @@ function gameLoop(){
     dot.x = clamp(newX,0 + w2, sceneWidth - w2)
     dot.y = clamp(newY,0 + h2, sceneHeight - h2)
 
-    wall.x = wall.x - 1;
+
+    if (score >= 5){
+        wall.scale.set(1, 2);
+    }
+
+    wall.x = wall.x - (1 + speed);
+
+    if  (rectsIntersect(dot, wall) == true){
+        end();
+	    return;
+    }
+
+    if (wall.x <= -20){
+        wall.x = 600;
+        wall.y = getRandom(-10,550);
+        increaseScoreBy(1);
+        speed += 0.5;
+    }
+
+    if (score > highscore){
+        localStorage.setItem(highKey, score);
+    }
+}
+
+function increaseScoreBy(value){
+    score += value;
+    scoreLabel.text = `Score     ${score}`;
+}
+
+function end(){
+    paused = true;
+
+    gameScene.removeChild(dot);
+
+    gameScene.removeChild(wall);
+
+    gameOverScene.visible = true;
+    gameScene.visible = false;
+}
+
+function reloadPage(){
+    location.reload();
 }
